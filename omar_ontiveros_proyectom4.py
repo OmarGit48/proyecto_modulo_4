6b02c38abb66168876194469dfd80b99e9398f8d
Omar Ontiveros <ontiveros2000@hotmail.com>
2023-07-12 00:48:36 -0600

    start the code, i have to instal requests

---
+++ omar_ontiveros_proyectom4.py
@@ -0,0 +1,10 @@
+
+# empezamos con la libreria requests
+import requests
+
+# pedimo sel nombre de un pokemon
+nombre = input("Escribe un nombre de Pokemon: ")
+
+# damos la liga o direccion dedon desacamos la información
+url =
+
