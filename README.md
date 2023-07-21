9cab6d9646894d7354212369be7201f0d76e370c
Omar Ontiveros <ontiveros2000@hotmail.com>
2023-07-21 04:51:45 -0600

    finish, i have it, but the road is long and I need to learn good habits

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 ##  The pokemon API
 
 1. Analyzing and reviewing the requierements, and the example
-2. Las clases lo dicen todo, esto es lo qeu requiero para mi idea de una App
-3. Ya no me estaba gustando pero ya lo encotnre muy interesante nuevamente
-4. Esto es mas qeu todo como saber en donde esta la informacion
-5. Muy interesante saber cuantas API´s puedo consumir
+2. Inside the class is all the information, this is what I require for my idea of an App
+3. I was very unmotivated, this module returned my interest
+4. This is for to know where is the information
+5. Very interesting tha we can get information of the API's
 6. I'm finish de project, i'm suseccsfull
 7. Is later and my english is not excelen, tomorrow will change language
-8. 
+8. my results are so good
+9. this is the image to get to the API
+![Magikarp](image.png) I hopw this is correct
+10. I up tow images of diferent form: first i take a screen shot and paste in the README file and i give 
+    name, the second form is from the file in my OS i put in the file of my repository
+11. I have a file text with the library with all weed for the code
+12. The images are a examples for the code's answer
+13. I forget to push and commit every change to the repositry
+14. Now i know i have it, thank you
--- omar_ontiveros_proyectom4.py
+++ omar_ontiveros_proyectom4.py
@@ -1,5 +1,5 @@
 
-# empezamos con la libreria requests
+# Start with the library
 import requests
 import json
 import os
@@ -7,55 +7,58 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from urllib.request import urlopen
 
-pokemon = input('pon un pokemon: ')
+pokemon = input("\nGive me a pokemon name: ") # asking for a pokemon name
 
 url = "https://pokeapi.co/api/v2/pokemon/" +pokemon
-respuesta = requests.get(url)
+answer = requests.get(url)
+"""Put the url where we get information to consult"""
 
-if respuesta.status_code != 200 :
-    print ("Algo salio mal, el pokemon no existe")
+if answer.status_code != 200 : # i use the status_code and print answer
+    print ("Something is wrong, the pokemon is not exist")
     exit()
     
-datos = respuesta.json()
+data = answer.json() # creat json's file
 
-name = datos["name"]
-print("This is the pokemon: ", name)
+name = data["name"]  # get the name of the pokemon and print
+print("\nThis is the pokemon: ", name)
 
-print("\nHis moves are: ")
-moves = datos["moves"]
+print("\nHis moves are: ") #get and print the pokemon's moves
+moves = data["moves"]
 for i in range(int(len(moves))):
     move = moves[i]["move"]["name"]
     print(move)
 
-weight = datos["weight"]
+weight = data["weight"] # get the pokemon's weight and print
 print("\nhis weight is: ", weight)
 
-height = datos["height"]
+height = data["height"] # get the pokemon's height and print
 print("\nhis height is:  ", height)
 
 print("\nThe abilities are: ")
-abilities = datos["abilities"]
+abilities = data["abilities"] # get the pokemon's abilities and print
 for u in range(int(len(abilities))):
     ability = abilities[u]["ability"]["name"]
     print(ability)
 
-types = datos["types"]
+types = data["types"] # get the pokemon's types and print
 for e in range(int(len(types))):
     type = types[e]["type"]["name"]
     print(type)
+    
 try:
-    url_imagen = datos["sprites"]["front_default"]    
-    imagen = Image.open(urlopen(url_imagen))
-    plt.title(datos["name"])
-    imgplot = plt.imshow(imagen)
+    url_imagen = data["sprites"]["front_default"] # try to get the pokemon's image if have it
+    image = Image.open(urlopen(url_imagen))
+    plt.title(data["name"])
+    imgplot = plt.imshow(image)
     plt.show()
 except:
-    print("El pokemon no tiene imagen")
+    print("We don't have image of the pokemon") # give answer when ww haven't image
     exit()
  
-if not os.path.exists("pokedex"):
+if not os.path.exists("pokedex"): # make a fiel pokedex if we don't have it
     os.makedirs("pokedex")    
 
+#get pokemon's information for save in the file pokedex
 pokemon_data = {
     "name": name,
     "moves": [move for move in moves],
@@ -68,4 +71,4 @@ pokemon_data = {
 with open("pokedex.json", "w") as json_file:
     json.dump(pokemon_data, json_file, indent=4)
 
-print(f"Datos del Pokémon guardados en el archivo 'pokedex/pokemon_data.json'")
+print(f"Pokemon data is saved 'pokedex/pokemon_data.json'")
